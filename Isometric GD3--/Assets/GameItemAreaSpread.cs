47344ca9903829b1b1f2dcd3aa9d0d7c4e46a106
Patr30t_Mark_Shope <160268@virtualwindows.co.za>
2021-03-01 16:08:27 +0100

    multiple object spawner

---
+++ Isometric GD3--/Assets/GameItemAreaSpread.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameItemAreaSpread : MonoBehaviour
+{
+    public GameObject itemsToSpread;
+    public int numItemsToSpawn = 20;
+
+    public float itemXSpread  =10;
+    public float itemYSpread = 0;
+    public float ItemZSpread = 10;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        for (int i = 0; i < numItemsToSpawn; i++)
+        {
+            SpreadItem();
+            
+        }
+
+    }
+
+void SpreadItem()
+{
+    Vector3 randPosition = new Vector3(Random.Range(-itemXSpread, itemXSpread), Random.Range(-ItemZSpread, ItemZSpread), Random.Range(-itemYSpread, itemYSpread)) + transform.position;
+    GameObject clone = Instantiate(itemsToSpread, randPosition , Quaternion.identity);
+}
+   
+}
