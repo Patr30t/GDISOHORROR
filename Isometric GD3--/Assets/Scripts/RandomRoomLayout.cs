2a2f059cc783085003eec525ccdd3d72764eb4ab
Patr30t_Mark_Shope <160268@virtualwindows.co.za>
2021-03-07 19:12:38 +0100

    RoomlayoutGenerator Added

---
+++ Isometric GD3--/Assets/Scripts/RandomRoomLayout.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRoomLayout : MonoBehaviour
+{
+    public List<GameObject> liGoSpawn = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject goToSpawn = liGoSpawn[Random.Range(0, liGoSpawn.Count)];
+        Instantiate(goToSpawn, transform.position, Quaternion.identity);
+        Destroy(gameObject);   
+    }
+
+   
+}
