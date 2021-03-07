6082194443d0492093fc6246d7c38b201a05f934
Patr30t_Mark_Shope <160268@virtualwindows.co.za>
2021-03-07 18:17:31 +0100

    simpler Procedural functionality

---
+++ Isometric GD3--/Assets/Scripts/LevelGeneration.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGeneration : MonoBehaviour
+{
+
+    public GameObject[] items;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int rand = Random.Range(0, items.Length);
+        Instantiate(items[rand], transform.position, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
