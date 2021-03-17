b7217e9df13d3580ff6e4db3dc2b2a23ee295fb6
Patr30t_Mark_Shope <160268@virtualwindows.co.za>
2021-03-17 13:50:43 +0100

    scriptable ob

---
+++ Isometric GD3--/Assets/Scripts/HealItems.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = " New HealItem", menuName = "HealItems")]
+public class HealItems : ScriptableObject
+{
+   
+   public new string name;
+   public string description;
+   
+   public int healPoints;
+   public int timePoints;
+}
+
+    
+
