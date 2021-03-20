2884f0248867c2585120ca8733b84a781eb271a0
Patr30t_Mark_Shope <160268@virtualwindows.co.za>
2021-03-20 08:36:55 +0100

    navmeshenemyworks

---
+++ Isometric GD3--/Assets/Scripts/EnemyControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyControl : MonoBehaviour
+{
+    public Transform playerPos;
+    protected NavMeshAgent enemyMesh;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyMesh = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemyMesh.SetDestination(playerPos.position);
+    }
+}
