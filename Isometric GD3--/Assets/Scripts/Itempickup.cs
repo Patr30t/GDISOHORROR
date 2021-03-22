e2c829f8008ac75ef8e72bf19d853e597ce7076f
Patr30t_Mark_Shope <160268@virtualwindows.co.za>
2021-03-22 22:36:24 +0100

    adding functionality

---
+++ Isometric GD3--/Assets/Scripts/Itempickup.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Itempickup : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            Destroy (gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Isometric GD3--/Assets/Scripts/PlayerDamage.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDamage : MonoBehaviour
+{
+    public int playerHealth = 40;
+    int damage = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+     private void OnCollisionEnter(Collision _collision)
+     {
+         if(_collision.gameObject.tag=="enemyAttack")
+         {
+             print ("playerHealth lost");
+         }
+     }
+        
+        
+}
